a497ccf507aada2e140e27ca0373361407638630
Gustavo Avila <gustavo.avila@gmail.com>
2017-07-24 22:07:41 -0500

    Creation of the iOS project

---
+++ HelloFucknWorld/HelloFucknWorld/Main.cs
@@ -0,0 +1,16 @@
+﻿using UIKit;
+
+namespace HelloFucknWorld
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            //small change to test github
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
