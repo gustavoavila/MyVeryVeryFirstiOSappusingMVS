a68a28740fc21c30d2462310cefde4640832343c
Gustavo Avila <gustavo.avila@gmail.com>
2017-07-24 22:10:32 -0500

    Inserting a comment

--- HelloFucknWorld/HelloFucknWorld/Main.cs
+++ HelloFucknWorld/HelloFucknWorld/Main.cs
@@ -10,6 +10,7 @@ namespace HelloFucknWorld
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             //small change to test github
+            //Another change
             UIApplication.Main(args, null, "AppDelegate");
         }
     }
